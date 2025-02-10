30eafa4e2ee1f638d0502dfa6b9f8fbdd14d3356
Feldwor <BoQsc@users.noreply.github.com>
2025-02-10 22:07:13 +0200

    Create server.py

---
+++ research_working/server.py
@@ -0,0 +1,11 @@
+# web_utils.py
+from aiohttp import web
+
+async def start_server():
+    app = web.Application()
+    return app
+
+if __name__ == "__main__":
+
+    web.run_app(start_server(), port=8000)
+
